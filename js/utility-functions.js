46cbce553d9eaf25c37b0761f388173e22352be2
nimotli <nimobitor@gmail.com>
2017-08-06 18:28:02 +0100

    msg

---
+++ js/utility-functions.js
@@ -0,0 +1,28 @@
+function loadSkills()
+{
+    $(document).ready(function()
+    {
+        $('#skills').load('./skills.html');
+    });
+}
+function loadPortfolio()
+{
+     $(document).ready(function()
+    {
+        $('#portfolio').load('./portfolio-items.html');
+    });
+}
+function loadProjects()
+{
+    $(document).ready(function()
+    {
+        $('#projects').load('./projects.html');
+    });
+}
+function loadContact()
+{
+    $(document).ready(function()
+    {
+        $('#contact').load('./contact.html');
+    });
+}
